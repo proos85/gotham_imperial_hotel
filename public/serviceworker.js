29320925706237e61286fd9ded5eb30ffa637cd0
Patrick Roos <patrick@point42.nl>
2019-01-19 20:34:02 +0100

    format document

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,44 +1,44 @@
 var CACHE_NAME = "gih-cache-v3";
 var CACHED_URLS = [
-  "/index-offline.html",
-  "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
-  "/css/gih-offline.css",
-  "/img/jumbo-background-sm.jpg",
-  "/img/logo-header.png"
+    "/index-offline.html",
+    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
+    "/css/gih-offline.css",
+    "/img/jumbo-background-sm.jpg",
+    "/img/logo-header.png"
 ];
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHED_URLS);
-    })
-  );
-});
-
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/index-offline.html");
-        }
-      });
-    })
-  );
-});
-
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (CACHE_NAME !== cacheName && cacheName.startsWith("gih-cache")) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(CACHED_URLS);
         })
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/index-offline.html");
+                }
+            });
+        })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (CACHE_NAME !== cacheName && cacheName.startsWith("gih-cache")) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
